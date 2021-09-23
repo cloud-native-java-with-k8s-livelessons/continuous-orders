464cadf7717ecb18ad2378b5cd3b9f72659e78d9
Josh Long <josh@joshlong.com>
2021-09-23 10:31:16 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 
-ROOT_DIR=$(cd $(dirname $0) && pwd)
-APP_NAME=feed-ingest-job
-PROJECT_ID=${GCLOUD_PROJECT}
-TAG_NAME=${1:-$(date +%s)}
-IMAGE_TAG="production${GITHUB_SHA:-}"
-GCR_IMAGE_NAME=gcr.io/${PROJECT_ID}/continuous-${APP_NAME}
-mvn -f ${ROOT_DIR}/../../pom.xml -DskipTests=true \
-  -e -Dspring.profiles.active=production  \
-  clean \
-  verify \
-  deploy \
-  spring-boot:build-image
-
-image_id=$(docker images -q $APP_NAME)
-docker tag "${image_id}" ${GCR_IMAGE_NAME}:latest
-docker tag "${image_id}" ${GCR_IMAGE_NAME}:${IMAGE_TAG}
-docker push ${GCR_IMAGE_NAME}:latest
-docker push ${GCR_IMAGE_NAME}:${IMAGE_TAG}
+HERE=$(cd $(dirname $0 && pwd ))
+NS=cnj
+APP_NAME=customers
+MANIFESTS_DIR=$HERE/k8s/manifests
+IMAGE_NAME=gcr.io/bootiful/${APP_NAME}:latest
+./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGE_NAME}
+docker push $IMAGE_NAME
+mkdir -p $MANIFESTS_DIR
+kubectl create ns $NS  -o yaml > $MANIFESTS_DIR/namespace.yaml
+kubectl -n $NS create deployment  --image=$IMAGE_NAME $APP_NAME -o yaml > $MANIFESTS_DIR/deployment.yaml
+kubectl -n $NS expose deployment $APP_NAME --port=8080 -o yaml > $MANIFESTS_DIR/service.yaml
+sleep 5
+kubectl -n $NS port-forward deployment/$APP_NAME 8080:8080  &
+curl -s localhost:8080/actuator/health/liveness | jq
+#kubectl logs -n cnj deployments/$APP_NAME
