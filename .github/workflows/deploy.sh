f8798d1d9c1016e7b139387399dab8857ce9ee5c
Josh Long <josh@joshlong.com>
2021-09-23 18:42:27 -0700

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,12 +3,10 @@ set -e
 HERE=$(cd $(dirname $0 ) && pwd )
 NS=cnj
 APP_NAME=customers
-MANIFESTS_DIR=$HERE/k8s/manifests
 IMAGE_NAME=gcr.io/bootiful/${APP_NAME}:latest
 cd $HERE/../..
 mvn clean package spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGE_NAME}
 docker push $IMAGE_NAME
-mkdir -p $MANIFESTS_DIR
 kubectl apply -f k8s/
 
 curl -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${GH_PAT}" \
